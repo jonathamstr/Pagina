88e71138e6bb5edf4ac1d9fc48b4808a7b6f43b3
jonathamstr <jonathanaguirreglez@gmail.com>
2016-07-27 11:04:17 -0700

    v0.1
    Consultas listas, Script corre el servidor y el tunel al mismo tiempo
    por medio de de hilos.
    El tiempo de ejercicion es aceptable.

--- pagina/main/views.py
+++ pagina/main/views.py
@@ -1,6 +1,56 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from os import getenv
+import pymssql
+import time
+
+server = "SERVERAVATTIA\AVATTIA"
+user = "sa"
+password = "aitva"
+
+
+
+def getData(db=None):
+    #conn = pymssql.connect(server, user, password, "AZTECA")
+    #cursor = conn.cursor()
+    #cursor.execute('SELECT * from dbo.p_clie')
+    conn = pymssql.connect(server,user,password, "FERBISBRASIL")
+    cursor = conn.cursor()
+    cursor.execute('SELECT top 10000 * from dbo.p_vede ')
+    result = cursor.fetchall()
+    conn.close()
+    return result
+
+def getVen(dsd,hst):
+    conn = pymssql.connect(server,user,password, "EjerciciosJonathan")
+    cursor = conn.cursor()
+    #cursor.execute('SELECT sum(dbo.p_vede.Importe) as Total,sum(dbo.p_vede.Cantidad) as Cantidad, dbo.p_vede.Producto, dbo.p_prod.desc from dbo.p_vede where fecha between '+dsd+' AND ' + hst +  )
+    cursor.execute('select distinct f.producto , s.VentaTotal, s.CantidadVendida, p.desc1 from dbo.p_vede as f inner join( \
+        select producto, sum(importe) as VentaTotal, sum(cantidad) as CantidadVendida from dbo.p_vede group by producto \
+        ) as s on f.producto = s.producto  inner join p_prod as p on p.producto = f.producto ORDER BY producto')
+    result = cursor.fetchall()
+    conn.close()
+    return result
 
 # Create your views here.
 
 def index(request):
     return render(request, 'main/home.html')
+
+def contact(request):
+    start = time.time()
+    info = getData()
+    result = time.time() - start
+    return render(request,'main/basic.html',{'content':info,'time':result})
+
+def consulta(request):
+    if request.method == 'GET':
+        return render(request, 'main/consulta.html')
+    elif request.method == 'POST':
+        dsd = request.POST['desde']
+        hst = request.POST['hasta']
+        start = time.time()
+        resulta  = getVen(dsd,hst)
+        start = time.time()-start
+        return render(request,'main/consulta.html',{'fch': [dsd ,hst],'result':resulta,'tiempo':start})
+        #return render_to_response('main/home.html',{},RequestContext(request))
--- pagina/manage.py
+++ pagina/manage.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 import os
 import sys
+import threading
+from queue import Queue
+
+print_lock = threading.Lock()
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pagina.settings")
@@ -8,3 +12,4 @@ if __name__ == "__main__":
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
+    
